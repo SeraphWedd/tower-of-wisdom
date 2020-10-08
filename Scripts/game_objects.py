bf673dfea36d0ee049b4ee89798132613c790219
J. E. Venturillo <seraphwedd18@gmail.com>
2020-10-08 13:43:56 +0800

    commit

--- Scripts/game_objects.py
+++ Scripts/game_objects.py
@@ -2,3 +2,25 @@
 File contraining all inherent game class objects.
 '''
 
+import pygame as pg
+
+class GameObject(object):
+    '''
+    The base GameObject class.
+    Position x and y are held in a global numpy array within
+    the main game engine.
+
+    Each GameObject instance will hold a unique index identifier
+    indicating their position on the numpy array.
+    '''
+    def __init__(self, GE, identity=0):
+        self.i = ident
+        self.game = GE
+        self.image = None
+        self.rect = None
+
+    def update(self, dt):
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import pygame as pg
 
+class GameEngine(object):
+    def __init__(self, width, height, fps):
+        pg.init()
+        self.screen = pg.display.set_mode((width, height))
+
+    def main_loop(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                    return
+
+            self.screen.fill((255, 255, 255))
+            pg.display.flip()
+
 if __name__ == "__main__":
-    pass
+    GE = GameEngine(800, 600, 60)
+    GE.main_loop()
